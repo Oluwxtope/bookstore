e5fd3c5ff4aa5750f51dac0962cfd1346559caf2
Emmanuel <oluwatopealofe@gmail.com>
2024-09-17 16:28:09 -0400

    Create express routes to handle server requests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,25 @@
 import express from "express";
 import config from "./../config/config.js";
 import mongoose from "mongoose";
+import { router } from "./../routes/routes.js";
 
-const main = async () => {
-    const app = express();
-    const port = config.server.port
-    const db = config.database.url
+const db = config.database.url
 
-    app.get('/', (_, res) => {
-        res.send('Hello, world!')
-    })
-
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`)
-    })
-
-    mongoose.connect(db).then(() => {
+mongoose
+    .connect(db)
+    .then(() => {
         console.log("MongoDB database connected")
     })
-}
+    .catch((err) => {
+        console.log("Error connecting to MongoDB database")
+        console.log(err)
+    })
 
+const app = express();
+const port = config.server.port
 
+app.use(router);
 
-main().catch((err) => {
-    console.log(err);
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`)
 })

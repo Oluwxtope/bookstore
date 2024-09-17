87e210eaecd1d9c1d3024b2b470985d7c14d1c01
Emmanuel <oluwatopealofe@gmail.com>
2024-09-17 13:10:04 -0400

    Setup moongoose and connect to mongodb database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import config from "./../config/config.json" with { type: "json" };
+import config from "./../config/config.js";
+import mongoose from "mongoose";
 
 const main = async () => {
     const app = express();
     const port = config.server.port
+    const db = config.database.url
 
     app.get('/', (_, res) => {
         res.send('Hello, world!')
@@ -12,7 +14,13 @@ const main = async () => {
     app.listen(port, () => {
         console.log(`Server is running on http://localhost:${port}`)
     })
+
+    mongoose.connect(db).then(() => {
+        console.log("MongoDB database connected")
+    })
 }
+
+
 
 main().catch((err) => {
     console.log(err);

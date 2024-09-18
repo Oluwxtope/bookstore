797338a191cb347398caab966821b253e5899a7f
Emmanuel <oluwatopealofe@gmail.com>
2024-09-18 01:03:47 -0400

    Setup page routes with react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import ShowBook from './pages/ShowBook'
+import CreateBook from './pages/CreateBooks'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<Home /> } />
+      <Route path='/books/create' element={<CreateBook /> } />
+      <Route path='/books/details/:id' element={<ShowBook /> } />
+      <Route path='/books/edit/:id' element={<EditBook /> } />
+      <Route path='/books/delete/:id' element={<DeleteBook /> } />
+    </Routes>
   )
 }
 
